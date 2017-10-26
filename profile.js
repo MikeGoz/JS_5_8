7e3ff1014bb9e74cb321ddc2c887c1d3b11ede64
MikeGoz <gozdz@onet.pl>
2017-10-26 18:43:56 +0200

    Task 5.8 correction

--- profile.js
+++ profile.js
@@ -30,44 +30,28 @@ function renderForm(req, res, locals) {
 }
 
 module.exports = function profile() {
-  
-  var router = express.Router();
+  const router = express.Router();
   router.use(cookieParser());
   router.use(bodyParser.urlencoded({ extended: true }));
   router.use(csurf({ cookie: true }));
-      
+
   router.all('/', function(req, res) {
-    profileForm.handle(req, {
-      success: function(form) {
-        req.user.givenName = form.data.givenName;
-        req.user.surname = form.data.surname;
-        req.user.customData.streetAddress = form.data.streetAddress;
-        req.user.customData.city = form.data.city;
-        req.user.customData.state = form.data.state;
-        req.user.customData.zip = form.data.zip;
-        req.user.customData.save();
-        req.user.save(function(err) {
-          if (err) {
-            if (err.developerMessage){
-              console.error(err);
-            }
-            renderForm(req, res, {
-              errors: [{
-                error: err.userMessage ||
-                err.message || String(err)
-              }]                              
-            });
-          } else {
-            renderForm(req, res, {
-               saved: true
-            });
+      profileForm.handle(req, {
+          success: function(form) {
+              req.user.givenName = form.data.givenName;
+            req.user.surname = form.data.surname;
+            req.user.streetAddress = form.data.streetAddress;
+            req.user.city = form.data.city;
+            req.user.state = form.data.state;
+            req.user.zip = form.data.zip;
+              renderForm(req, res, {
+                  saved: true
+              });
+          },
+          empty: function() {
+              renderForm(req, res);
           }
-        });
-      },
-      empty: function() {
-         renderForm(req, res);
-      }
-    });
-  });      
+      });
+  });
   return router;
 };
